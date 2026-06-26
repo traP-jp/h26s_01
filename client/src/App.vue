ae36d10bb1722cb1cbceceea79649bf742eff991
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-06-27 03:08:43 +0900

    chore: npm scripts を整理

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <h1>h26s_01</h1>
+  </main>
 </template>
 
 <style scoped></style>
