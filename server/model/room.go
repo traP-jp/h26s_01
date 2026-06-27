8d4bfdb900fccc4f4383b91c5ff35d83868cc841
Takumi Akimoto <noreply@akimo.dev>
2026-06-27 11:08:27 +0900

    server: Add models (#23)

--- server/model/room.go
+++ server/model/room.go
@@ -1,1 +1,15 @@
 package model
+
+import "github.com/google/uuid"
+
+type Room struct {
+	ID      uuid.UUID    `db:"id"`
+	Name    string       `db:"name"`
+	Status  string       `db:"status"`
+	Members []RoomMember `db:"-"`
+}
+
+type RoomMember struct {
+	RoomID uuid.UUID `db:"room_id"`
+	UserID string    `db:"user_id"`
+}
--- server/model/stroke.go
+++ server/model/stroke.go
@@ -1,1 +1,12 @@
 package model
+
+import "github.com/google/uuid"
+
+type Stroke struct {
+	ID     uuid.UUID `db:"id"`
+	TurnID uuid.UUID `db:"turn_id"`
+	X1     float64   `db:"x1"`
+	Y1     float64   `db:"y1"`
+	X2     float64   `db:"x2"`
+	Y2     float64   `db:"y2"`
+}
