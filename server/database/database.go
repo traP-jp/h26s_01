a34787b71777fa24af8d9e42bb872bb804628194
Takumi Akimoto <noreply@akimo.dev>
2026-06-27 10:36:18 +0900

    server: Setup repository

--- server/database/database.go
+++ server/database/database.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func Setup(mysqlConfig *mysql.Config) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("mysql", mysqlConfig.FormatDSN())
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	if err := migrateTables(db.DB); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- server/database/migration.go
+++ server/database/migration.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var migrations embed.FS
+
+func migrateTables(db *sql.DB) error {
+	goose.SetBaseFS(migrations)
+
+	if err := goose.SetDialect("mysql"); err != nil {
+		return fmt.Errorf("setting dialect: %w", err)
+	}
+
+	if err := goose.Up(db, "migrations"); err != nil {
+		return fmt.Errorf("applying migrations: %w", err)
+	}
+
+	return nil
+}
