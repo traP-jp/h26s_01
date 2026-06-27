f305d40565d02a00a5249fc3cf17ba97741d9fe4
yadorigi <293710634+yadorigi-y@users.noreply.github.com>
2026-06-27 17:02:33 +0900

    feat: usericonを実装

---
+++ client/src/components/common/UserIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ traQid: string; size: 'small' | 'medium' | 'large' }>();
+</script>
+
+<template>
+  <img
+    :src="`https://q.trap.jp/api/v3/public/icon/${traQid}`"
+    alt="アイコン"
+    :class="{
+      'w-4 h-4': size == 'small',
+      'w-12 h-12': size == 'medium',
+      'w-36 h-36': size == 'large',
+    }"
+  />
+</template>
