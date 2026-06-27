d86be1d464c73805b61b2afe81a90693a02dae0e
shio0418 <furumiya.s.aa@m.titech.ac.jp>
2026-06-28 00:28:28 +0900

    feat: GetCurrentRoundByRoomID関数を実装

--- server/repository/round.go
+++ server/repository/round.go
@@ -1,1 +1,19 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/traP-jp/h26s_01/server/model"
+)
+
+func (r *Repository) GetCurrentRoundByRoomID(ctx context.Context, roomID uuid.UUID) (model.Round, error) {
+	var round model.Round
+	query := "SELECT r.* FROM games g JOIN rounds r ON r.id = g.current_round_id WHERE g.room_id = ?"    
+
+	if err := r.db.GetContext(ctx, &round, query, roomID); err != nil {
+		return model.Round{}, err
+	}
+
+	return round, nil
+}
