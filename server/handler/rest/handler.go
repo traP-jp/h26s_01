2e954cde7ff888898244112ad4abdf2e34f2594a
Takumi Akimoto <noreply@akimo.dev>
2026-06-27 11:18:56 +0900

    handler: Add a helper method to get authenticated user

--- server/handler/rest/handler.go
+++ server/handler/rest/handler.go
@@ -1,6 +1,10 @@
 package rest
 
-import "github.com/traP-jp/h26s_01/server/repository"
+import (
+	"github.com/labstack/echo/v5"
+	"github.com/traP-jp/h26s_01/server/model"
+	"github.com/traP-jp/h26s_01/server/repository"
+)
 
 type Handler struct {
 	repo *repository.Repository
@@ -10,4 +14,10 @@ func New(repo *repository.Repository) *Handler {
 	return &Handler{
 		repo: repo,
 	}
+}
+
+func (h *Handler) getLoggedInUser(c *echo.Context) (*model.User, error) {
+	userID := c.Request().Header.Get("X-Forwarded-User")
+
+	return h.repo.GetOrCreateUser(c.Request().Context(), userID)
 }
--- server/repository/user.go
+++ server/repository/user.go
@@ -1,1 +1,37 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/traP-jp/h26s_01/server/model"
+)
+
+func (r *Repository) GetOrCreateUser(ctx context.Context, userID string) (*model.User, error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer tx.Rollback()
+
+	var user model.User
+
+	if err = tx.
+		QueryRowContext(ctx, "SELECT id FROM users WHERE id = ?", userID).
+		Scan(&user.ID); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			if err = tx.
+				QueryRowContext(ctx, "INSERT INTO users (id) VALUES (?) RETURNING id", userID).
+				Scan(&user.ID); err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	}
+
+	return &user, tx.Commit()
+}
