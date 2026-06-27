55329176718db54b50faa505e178296fcf8db9ff
y33550336 <u5yuugoo@gmail.com>
2026-06-27 10:23:56 +0900

    fix: FOREIGN KEYを別に分けた

--- server/database/migrations/1_schema.sql
+++ server/database/migrations/1_schema.sql
@@ -1,62 +1,71 @@
 -- +goose Up
 
 CREATE TABLE IF NOT EXISTS users(
-    id VARCHAR(32) NOT NULL PRIMARY KEY,
+    id VARCHAR(32) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS rooms(
     id UUID NOT NULL PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
-    status VARCHAR(16) NOT NULL,
+    status VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS room_members(
-    room_id UUID NOT NULL FOREIGN KEY REFERENCES rooms(id),
-    user_id VARCHAR(32) NOT NULL FOREIGN KEY REFERENCES users(id),
-    PRIMARY KEY(room_id, user_id)
+    room_id UUID NOT NULL,
+    user_id VARCHAR(32) NOT NULL,
+    PRIMARY KEY(room_id, user_id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS games(
-    room_id UUID NOT NULL FOREIGN KEY REFERENCES rooms(id),
-    current_round_id UUID DEFAULT NULL FOREIGN KEY REFERENCES rounds(id),
-    PRIMARY KEY(room_id)
+    room_id UUID NOT NULL PRIMARY KEY,
+    current_round_id UUID DEFAULT NULL,
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 CREATE TABLE IF NOT EXISTS game_kanjies(
     id UUID NOT NULL PRIMARY KEY,
-    game_id UUID NOT NULL FOREIGN KEY REFERENCES games(room_id),
+    game_id UUID NOT NULL,
     character VARCHAR(1) NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(room_id)
 );
 
 CREATE TABLE IF NOT EXISTS rounds(
     id UUID NOT NULL PRIMARY KEY,
-    game_id UUID NOT NULL FOREIGN KEY REFERENCES games(room_id),
+    game_id UUID NOT NULL,
     round_index TINYINT UNSIGNED NOT NULL,
-    current_turn_id UUID DEFAULT NULL FOREIGN KEY REFERENCES turns(id),
-    guesser_id VARCHAR(32) NOT NULL FOREIGN KEY REFERENCES users(id),
-    kanji_id UUID NOT NULL FOREIGN KEY REFERENCES game_kanjies(id),
+    current_turn_id UUID DEFAULT NULL,
+    guesser_id VARCHAR(32) NOT NULL,
+    kanji_id UUID NOT NULL,
     started_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(room_id),
+    FOREIGN KEY (guesser_id) REFERENCES users(id),
+    FOREIGN KEY (kanji_id) REFERENCES game_kanjies(id)
 );
 
 CREATE TABLE IF NOT EXISTS turns(
     id UUID NOT NULL PRIMARY KEY,
-    round_id UUID NOT NULL FOREIGN KEY REFERENCES rounds(id),
+    round_id UUID NOT NULL,
     turn_index TINYINT UNSIGNED NOT NULL,
-    drawer_id VARCHAR(32) NOT NULL FOREIGN KEY REFERENCES users(id),
+    drawer_id VARCHAR(32) NOT NULL,
+    FOREIGN KEY (round_id) REFERENCES rounds(id),
+    FOREIGN KEY (drawer_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS strokes(
     id UUID NOT NULL PRIMARY KEY,
-    turn_id UUID NOT NULL FOREIGN KEY REFERENCES turns(id),
+    turn_id UUID NOT NULL,
     x1 DOUBLE UNSIGNED NOT NULL,
     y1 DOUBLE UNSIGNED NOT NULL,
     x2 DOUBLE UNSIGNED NOT NULL,
     y2 DOUBLE UNSIGNED NOT NULL,
+    FOREIGN KEY (turn_id) REFERENCES turns(id)
 );
 
 CREATE TABLE IF NOT EXISTS round_results(
-    round_id UUID NOT NULL FOREIGN KEY REFERENCES rounds(id),
+    round_id UUID NOT NULL PRIMARY KEY,
     guesser_answer VARCHAR(1) NOT NULL,
     time_ms INT UNSIGNED NOT NULL,
-    PRIMARY KEY(round_id)
-)
+    FOREIGN KEY (round_id) REFERENCES rounds(id)
+);
