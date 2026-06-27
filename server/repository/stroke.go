c15d45848e7b4dad25e4bc07462837a645b33127
shio0418 <furumiya.s.aa@m.titech.ac.jp>
2026-06-27 18:41:19 +0900

    feat: SaveStroke関数を実装

--- server/repository/stroke.go
+++ server/repository/stroke.go
@@ -1,1 +1,21 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+func (r *Repository) SaveStroke(ctx context.Context, turnId uuid.UUID, x1, y1, x2, y2 float64) error {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return err
+	}
+
+	query := "INSERT INTO strokes (id, turn_id, x1, y1, x2, y2) VALUES (?, ?, ?, ?, ?, ?)"
+	_, err = r.db.ExecContext(ctx, query, id, turnId, x1, y1, x2, y2)
+	if err != nil {
+		return err
+	}
+	return nil
+}
