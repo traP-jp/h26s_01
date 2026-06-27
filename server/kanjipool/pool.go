0c352f316439df168b8563e48e44dd9e67a4ef5a
古宮詩桜 <furumiya.s.aa@m.titech.ac.jp>
2026-06-27 13:11:14 +0900

    feat: kanjiのparse関数を追加

--- server/kanjipool/pool.go
+++ server/kanjipool/pool.go
@@ -1,14 +1,36 @@
 package kanjipool
 
-import _ "embed"
+import (
+	"bytes"
+	_ "embed"
+	"encoding/csv"
+	"strconv"
+)
 
 //go:embed kanji.csv
 var kanjiCSV []byte
 
 type Kanji struct {
-	
+	Char   string
+	Stroke int
 }
 
-func parseKanji() {
+func parseKanji() ([]Kanji, error) {
+	r := csv.NewReader(bytes.NewReader(kanjiCSV))
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
 
+	var kanjis []Kanji
+	for _, record := range records[1:] {
+		kanji := record[1]
+		stroke, err := strconv.Atoi(record[3])
+		if err != nil {
+			return nil, err
+		}
+
+		kanjis = append(kanjis, Kanji{kanji, stroke})
+	}
+	return kanjis, nil
 }
