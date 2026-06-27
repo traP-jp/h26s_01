41b1698ed982c66ef9cfee37bddf4b7b94332ca6
Takumi Akimoto <noreply@akimo.dev>
2026-06-27 10:55:04 +0900

    handler: Add stubs for REST endpoints (#21)
    * handler: Add stubs for REST endpoints
    
    * Add argument names

--- server/handler/rest/handler.go
+++ server/handler/rest/handler.go
@@ -1,1 +1,13 @@
 package rest
+
+import "github.com/traP-jp/h26s_01/server/repository"
+
+type Handler struct {
+	repo *repository.Repository
+}
+
+func New(repo *repository.Repository) *Handler {
+	return &Handler{
+		repo: repo,
+	}
+}
--- server/handler/rest/room.go
+++ server/handler/rest/room.go
@@ -1,1 +1,11 @@
 package rest
+
+import "github.com/labstack/echo/v5"
+
+func (h *Handler) GetRooms(c *echo.Context) error {
+	return nil
+}
+
+func (h *Handler) PostRoom(c *echo.Context) error {
+	return nil
+}
