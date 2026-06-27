892d207f88381b391ac8d16de3d86c419575a69d
y33550336 <u5yuugoo@gmail.com>
2026-06-27 23:03:53 +0900

    feat: answer:submitイベントの処理の実装

--- server/handler/socketio/answer.go
+++ server/handler/socketio/answer.go
@@ -1,10 +1,52 @@
 package socketio
 
 import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/traP-jp/h26s_01/server/api"
 	"github.com/zishang520/socket.io/servers/socket/v3"
 )
 
 func (h *Handler) handleAnswerSubmit(socket *socket.Socket, event api.AnswerSubmitEvent) error {
+	user, err := h.getLoggedInUser(socket)
+	if err != nil {
+		return err
+	}
+
+	rooms := socket.Rooms()
+	myID := string(socket.Id())
+	var roomID uuid.UUID
+
+	if rooms != nil {
+		for _, room := range rooms.Keys() {
+			if string(room) != myID {
+				roomID, err = uuid.Parse(string(room))
+
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		}
+	}
+	if roomID == uuid.Nil {
+		return errors.New("roomID is empty")
+	}
+
+	currentRoundID, err := h.repo.GetRoundByRoomID(socket.Request().Context(), roomID.String())
+	if err != nil {
+		return err
+	}
+	currentRoundUUID := currentRoundID.UUID
+
+	current_time := time.Now()
+
+	if err := h.repo.SubmitAnswer(socket.Request().Context(), currentRoundUUID, current_time, event.Answer); err != nil {
+		return err
+	}
+
 	return nil
 }
