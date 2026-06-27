603783d33bfb74c0f45e2dc1d413a2f39de4bd2c
shio0418 <furumiya.s.aa@m.titech.ac.jp>
2026-06-28 00:26:33 +0900

    feat: CountRoundResult関数を実装

--- server/repository/round_result.go
+++ server/repository/round_result.go
@@ -14,3 +14,23 @@ func (r *Repository) GetRoundResult(ctx context.Context, roundId uuid.UUID) (mod
 	}
 	return roundResult, nil
 }
+
+func (r *Repository) CountRoundResult(ctx context.Context, gameId uuid.UUID) (correct int, incorrect int, err error) {
+	query := `SELECT
+	SUM(rr.guesser_answer = gk.character) AS correct,
+	SUM(rr.guesser_answer != gk.character) AS incorrect
+	FROM rounds r
+	JOIN round_results rr ON rr.round_id = r.id
+	JOIN game_kanjies gk ON gk.id = r.kanji_id
+	WHERE r.game_id = ?`
+
+	var result struct {
+		Correct   int `db:"correct"`
+		Incorrect int `db:"incorrect"`
+	}
+	
+	if err := r.db.GetContext(ctx, &result, query, gameId); err != nil {
+		return 0, 0, err
+	}
+	return result.Correct, result.Incorrect, nil
+}
