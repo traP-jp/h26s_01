d6faf6afa2edc0da4705d01fd4cf20e1b28569e3
shio0418 <furumiya.s.aa@m.titech.ac.jp>
2026-06-27 23:44:05 +0900

    feat: GetRoundResult関数を実装

--- server/repository/round_result.go
+++ server/repository/round_result.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/traP-jp/h26s_01/server/model"
+)
+
+func (r *Repository) GetRoundResult(ctx context.Context, roundId uuid.UUID) (model.RoundResult, error) {
+	var roundResult model.RoundResult
+	if err := r.db.GetContext(ctx, &roundResult, "SELECT * FROM round_results WHERE round_id = ?", roundId); err != nil {
+		return model.RoundResult{}, err
+	}
+	return roundResult, nil
+}
