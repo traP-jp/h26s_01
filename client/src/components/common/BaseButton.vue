4baf00d462025b87bdc4c1a28ca40c981d090570
a-cuppa <169031030+a-cuppa@users.noreply.github.com>
2026-06-27 16:05:14 +0900

    feat: 基本ボタンを実装

---
+++ client/src/components/common/BaseButton.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    variant: 'primary' | 'secondary';
+    disabled?: boolean;
+  }>(),
+  { disabled: false },
+);
+</script>
+
+<template>
+  <button
+    type="button"
+    :disabled="props.disabled"
+    class="text-2xl px-12 py-2 border-4 border-primary transition"
+    :class="{
+      'opacity-40 dark': props.disabled,
+      'cursor-pointer': !props.disabled,
+      'text-base bg-primary': props.variant == 'primary',
+      'hover:text-primary hover:bg-base': props.variant == 'primary' && !props.disabled,
+      'text-primary bg-base': props.variant == 'secondary',
+      'hover:text-base hover:bg-primary': props.variant == 'secondary' && !props.disabled,
+    }"
+  >
+    <slot />
+  </button>
+</template>
