10299bc0bdcdfa2299374b991ac9b91402baae2f
a-cuppa <169031030+a-cuppa@users.noreply.github.com>
2026-06-28 13:10:41 +0900

    WIP:待機部屋を実装

--- client/src/views/WaitingRoomView.vue
+++ client/src/views/WaitingRoomView.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
 import BaseButton from '@/components/common/BaseButton.vue';
 import TheHeader from '@/components/common/TheHeader.vue';
 import UserIcon from '@/components/common/UserIcon.vue';
+import { useGameSocket } from '@/composables/useGameSocket';
+import { useRoomSocket } from '@/composables/useRoomSocket';
 import { useRoomStore } from '@/stores/room';
 import { toKanjiNumber } from '@/utils/to-kanji-number';
 
 const { currentRoom } = storeToRefs(useRoomStore());
+const { sendReady } = useRoomSocket();
+
+const isReady = ref(false);
+
+const handleClick = () => {
+  const res = sendReady();
+  if (res) {
+    isReady.value = true;
+  }
+};
 </script>
 
 <template>
@@ -19,14 +32,19 @@ const { currentRoom } = storeToRefs(useRoomStore());
         <p class="text-4xl text-primary">{{ toKanjiNumber(currentRoom.members.length) }}人部屋</p>
       </div>
       <div class="grid grid-cols-4 gap-x-28 gap-y-12 mt-16 ml-32 w-fit">
-        <div v-for="n in 7" :key="n" class="flex flex-col items-center w-fit gap-4">
-          <UserIcon tra-qid="" size="large" />
-          <p class="text-3xl text-center text-primary">traQid</p>
+        <div
+          v-for="member in currentRoom.members"
+          :key="member.id"
+          class="flex flex-col items-center w-fit gap-4"
+          :class="{ 'opacity-30': !member.isReady }"
+        >
+          <UserIcon :tra-qid="member.id" size="large" />
+          <p class="text-3xl text-center text-primary">{{ member.id }}</p>
         </div>
       </div>
     </div>
     <div class="fixed bottom-16 right-24">
-      <BaseButton variant="primary">準備完了</BaseButton>
+      <BaseButton v-if="!isReady" variant="primary" @btn-click="handleClick">準備完了</BaseButton>
     </div>
   </div>
 </template>
