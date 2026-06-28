054a9a2bff1154e4493de775c2af02e884ee8dcc
Takumi Akimoto <noreply@akimo.dev>
2026-06-28 09:38:29 +0900

    handler: Implement `client:disconnected` (#67)
    * handler: Implement `client:disconnected`
    
    * Discard changes around round creation

--- server/repository/round.go
+++ server/repository/round.go
@@ -9,7 +9,7 @@ import (
 
 func (r *Repository) GetCurrentRoundByRoomID(ctx context.Context, roomID uuid.UUID) (model.Round, error) {
 	var round model.Round
-	query := "SELECT r.* FROM games g JOIN rounds r ON r.id = g.current_round_id WHERE g.room_id = ?"    
+	query := "SELECT r.* FROM games g JOIN rounds r ON r.id = g.current_round_id WHERE g.room_id = ?"
 
 	if err := r.db.GetContext(ctx, &round, query, roomID); err != nil {
 		return model.Round{}, err
