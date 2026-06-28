9f87244f99200566dc31448900778db51e5632a7
a-cuppa <169031030+a-cuppa@users.noreply.github.com>
2026-06-28 12:34:50 +0900

    feat: 各初期化処理を実施

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, onUnmounted } from 'vue';
+
+import { useGameSocket } from './composables/useGameSocket';
+import { useLobbySocketEvents } from './composables/useLobbySocketEvents';
+import { useRoomSocket } from './composables/useRoomSocket';
+import { useSession } from './composables/useSession';
+
+const lobbySocketEvents = useLobbySocketEvents();
+const roomSocket = useRoomSocket();
+const gameSocket = useGameSocket();
+const session = useSession();
+
+onMounted(() => {
+  lobbySocketEvents.register();
+  roomSocket.register();
+  gameSocket.register();
+  session.initializeSession();
+});
+
+onBeforeUnmount(() => {
+  lobbySocketEvents.cleanup();
+  roomSocket.cleanup();
+  gameSocket.cleanup();
+  session.initializeSession();
+});
+</script>
 
 <template>
   <main>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/mock-check">Mock Check</RouterLink>
-      <RouterLink to="/socket-sample">Socket Sample</RouterLink>
-    </nav>
-
     <RouterView />
   </main>
 </template>
